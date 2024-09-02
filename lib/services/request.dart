3157c305f3c5853b0a096bdbb325902edc3a062f
Reynaldo GT <larz.dev@gmail.com>
2024-09-01 22:36:11 -0400

    added request services files

---
+++ lib/services/request.dart
@@ -0,0 +1,104 @@
+part of 'services.dart';
+
+enum RequestType {
+  get,
+  post,
+  put,
+  delete,
+}
+
+class Request {
+  String uri = '${Environment.serverApi}/';
+  static Future<http.Response?> sendRequest(
+    RequestType method,
+    String url, {
+    Map<String, String>? body,
+  }) async {
+    final headers = {
+      'Content-Type': 'application/json',
+    };
+    Uri uri = Uri.parse('${Environment.serverApi}/$url');
+    String requestBody = body != null ? jsonEncode(body) : '{}';
+    late http.Response res;
+    switch (method) {
+      case RequestType.get:
+        res = await http.get(uri);
+        break;
+      case RequestType.post:
+        res = await http.post(uri, body: requestBody, headers: headers);
+        break;
+      case RequestType.put:
+        res = await http.put(uri, body: requestBody, headers: headers);
+        break;
+      case RequestType.delete:
+        res = await http.delete(uri);
+    }
+    return res;
+  }
+
+  static Future<http.Response?> sendRequestWithToken(
+    RequestType method,
+    String url, {
+    Map<String, dynamic>? body,
+  }) async {
+    final token = await KeyValueStorageServiceImpl().getValue<String>('token');
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+
+    final Uri uri = Uri.parse('${Environment.serverApi}/$url');
+
+    String requestBody = body != null ? jsonEncode(body) : '{}';
+    late http.Response res;
+    switch (method) {
+      case RequestType.get:
+        res = await http.get(uri, headers: headers);
+        break;
+      case RequestType.post:
+        res = await http.post(uri, body: requestBody, headers: headers);
+        break;
+      case RequestType.put:
+        res = await http.put(uri, body: requestBody, headers: headers);
+        break;
+      case RequestType.delete:
+        res = await http.delete(uri, headers: headers);
+    }
+    return res;
+  }
+
+  /* 
+  TODO use this method
+  static Future<http.Response> sendRequestWithFile(
+    RequestType requestType,
+    String url,
+    String filePath, {
+    Map<String, String>? body,
+  }) async {
+    late http.Response res;
+    final token = await KeyValueStorageServiceImpl().getValue<String>('token');
+    final Uri uri = Uri.parse('${Environment.serverApi}/$url');
+
+    final mimeType = mime(filePath)!.split('/');
+    final headers = {
+      'Authorization': 'Bearer $token',
+    };
+    final uploadFile = await http.MultipartFile.fromPath(
+      'file',
+      filePath,
+      contentType: MediaType(mimeType[0], mimeType[1]),
+    );
+
+    final uploadPostRequest = http.MultipartRequest(
+      requestType == RequestType.post ? 'POST' : 'PUT',
+      uri,
+    );
+    uploadPostRequest.headers.addAll(headers);
+    uploadPostRequest.files.add(uploadFile);
+    if (body != null) uploadPostRequest.fields.addAll(body);
+    final streamResponse = await uploadPostRequest.send();
+    res = await http.Response.fromStream(streamResponse);
+
+    return res;
+  } */
+}
---
+++ lib/services/services.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:psico_educativa_app/config/environment.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+part 'key_value/key_value_storage_service_impl.dart';
+part 'key_value/key_value_storage_service.dart';
+
+part 'request.dart';
